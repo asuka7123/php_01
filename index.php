0c209cb1d81cbc222301ed1645542de0523c7155
asuka7123 <80142146+asuka7123@users.noreply.github.com>
2021-03-16 04:42:24 -0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+$hands=["グー","チョキ","パー"];
+$picts=['janken_gu','janken_choki','janken_pa'];
+$results=["あいこ","あなたの負け","あなたの勝ち！"];
+
+if(isset($_POST["hand"])){
+  $userHand=(int)$_POST["hand"];
+  $pcHand=rand(0,count($hands)-1);
+ }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/main.css?v=2">
+<title>じゃんけんぽん</title>
+</head>
+
+<body>
+
+<p id=title>じゃんけんゲーム</p>
+
+<script>
+  const CLASSNAME = "-visible";
+  const TIMEOUT = 1500;
+  const $target = $(".title");
+
+  setInterval(() => {
+    $target.addClass(CLASSNAME);
+    setTimeout(() => {
+      $target.removeClass(CLASSNAME);
+    }, TIMEOUT);
+  }, TIMEOUT * 2);
+</script>
+
+<form method="post">
+<?php for($i=0;$i<count($hands);$i++):?>
+  <?php $checked=isset($userHand) && $userHand===$i ? "checked":"";?>
+  <input type="radio" name="hand" value="<?=$i?>" <?=$checked?>><?=$hands[$i]?><br>
+<?php endfor;?>
+  <button type="submit" id="jankenpon">じゃんけんぽん！</button>
+</form>
+<?php if(isset($_POST['hand'])):?>
+  <div>
+    <img src="img/<?=$picts[$userHand]?>.png">
+    <img src="img/<?=$picts[$pcHand]?>.png">
+  </div>
+  <p><?=$results[($userHand + 3 -$pcHand) % 3]?></p>
+<?php endif;?>
+
+</body>
+</html>
